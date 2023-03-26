44902485578e9e8df3c9900c90d9cf1b4a3ea34c
VIKING <viking.yfy@qq.com>
2023-03-26 15:42:36 +0800

    Update Plugins.sh

--- Update/Plugins.sh
+++ Update/Plugins.sh
@@ -13,16 +13,16 @@ git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/
 cd ./OpenClash/luci-app-openclash/root/etc/openclash
 mkdir ./core && cd ./core
 
-export TUN_VER=$(curl -s $CORE_VER | sed -n "2p")
-curl -SsL -o ./tun.gz "$CORE_TUN"-"$TUN_VER".gz
+export TUN_VER=$(curl -fsL $CORE_VER | sed -n "2p")
+curl -fsL -o ./tun.gz "$CORE_TUN"-"$TUN_VER".gz
 gzip -d ./tun.gz
 mv ./tun ./clash_tun
 
-curl -SsL -o ./meta.tar.gz $CORE_MATE
+curl -fsL -o ./meta.tar.gz $CORE_MATE
 tar -zxf ./meta.tar.gz
 mv ./clash ./clash_meta
 
-curl -SsL -o ./dev.tar.gz $CORE_DEV
+curl -fsL -o ./dev.tar.gz $CORE_DEV
 tar -zxf ./dev.tar.gz
 
 chmod +x ./clash*
