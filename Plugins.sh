6ea74260d6ea866218582abb8b891dd07ab2368c
VIKING <viking.yfy@qq.com>
2023-03-28 19:52:22 +0800

    Add files via upload

---
+++ Plugins.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#Argon Theme
+git clone --depth=1 --single-branch --branch "18.06" https://github.com/jerrykuku/luci-theme-argon.git
+#Argon Config
+git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-app-argon-config.git
+#Design Theme
+git clone --depth=1 --single-branch https://github.com/gngpp/luci-theme-design.git
+#Design Config
+git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
+#OpenClash
+git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/OpenClash.git
+
+#OpenClash Core
+export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
+export CORE_TUN=https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux
+export CORE_DEV=https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux
+export CORE_MATE=https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux
+
+cd ./OpenClash/luci-app-openclash/root/etc/openclash
+mkdir ./core && cd ./core
+
+export TUN_VER=$(curl -sfL $CORE_VER | sed -n "2p")
+curl -sfL -o ./tun.gz "$CORE_TUN"-"$DEVICE_TYPE"-"$TUN_VER".gz
+gzip -d ./tun.gz
+mv ./tun ./clash_tun
+
+curl -sfL -o ./meta.tar.gz "$CORE_MATE"-"$DEVICE_TYPE".tar.gz
+tar -zxf ./meta.tar.gz
+mv ./clash ./clash_meta
+
+curl -sfL -o ./dev.tar.gz "$CORE_DEV"-"$DEVICE_TYPE".tar.gz
+tar -zxf ./dev.tar.gz
+
+chmod +x ./clash*
+rm -rf ./*.gz
