421547bc936fa7a4f90b7dbb338cf9742fd1a73e
VIKING <viking.yfy@qq.com>
2023-03-31 12:33:50 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -3,9 +3,9 @@
 #删除冲突主题
 rm -rf $(find ./feeds/luci/ -type d \( -name "*-argon*" -or -name "*-design*" \))
 #默认主机名
-sed -i "s/OpenWrt/$DEVICE_NAME/g" ./package/base-files/files/bin/config_generate
+sed -i "s/OpenWrt/$OWRT_NAME/g" ./package/base-files/files/bin/config_generate
 #默认IP地址
-sed -i "s/192.168.1.1/$DEVICE_IP/g" ./package/base-files/files/bin/config_generate
+sed -i "s/192.168.1.1/$OWRT_IP/g" ./package/base-files/files/bin/config_generate
 #默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-design/g' ./feeds/luci/collections/luci/Makefile
 #默认时间格式
