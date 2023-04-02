f6076eb49addfd80b925aea9acc6e6f30a1599b4
VIKING <viking.yfy@qq.com>
2023-04-02 17:22:21 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -16,21 +16,19 @@ export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/c
 export CORE_TUN=https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux
 export CORE_DEV=https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux
 export CORE_MATE=https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux
+export CORE_TYPE=$(if [ "$OWRT_TYPE" = "X86" ]; then echo "amd64"; else echo "arm64"; fi)
+export TUN_VER=$(curl -sfL $CORE_VER | sed -n "2p")
 
 cd ./OpenClash/luci-app-openclash/root/etc/openclash
 mkdir ./core && cd ./core
 
-export TUN_VER=$(curl -sfL $CORE_VER | sed -n "2p")
 curl -sfL -o ./tun.gz "$CORE_TUN"-"$CORE_TYPE"-"$TUN_VER".gz
-gzip -d ./tun.gz
-mv ./tun ./clash_tun
+gzip -d ./tun.gz && mv ./tun ./clash_tun
 
 curl -sfL -o ./meta.tar.gz "$CORE_MATE"-"$CORE_TYPE".tar.gz
-tar -zxf ./meta.tar.gz
-mv ./clash ./clash_meta
+tar -zxf ./meta.tar.gz && mv ./clash ./clash_meta
 
 curl -sfL -o ./dev.tar.gz "$CORE_DEV"-"$CORE_TYPE".tar.gz
 tar -zxf ./dev.tar.gz
 
-chmod +x ./clash*
-rm -rf ./*.gz
+chmod +x ./clash* ; rm -rf ./*.gz
