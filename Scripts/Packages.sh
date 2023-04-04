db8c45d000867d5524cf00573b97524fe0199618
VIKING <viking.yfy@qq.com>
2023-04-04 23:57:34 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -5,7 +5,7 @@ for var in $OWRT_TYPE; do
   for file in $(find ./bin/targets/ -type f -iregex ".*\($var\).*\.\(bin\|iso\|vmdk\|img.gz\)"); do
     export ext=$(basename "$file" | cut -d '.' -f 2-3)
     export name=$(basename "$file" | cut -d '.' -f1 | grep -io "\($var\).*")
-    export fullname="$OWRT_NAME"_"$name"_"$OWRT_DATE"."$ext"
-    cp -rf "$file" ./upload/"$fullname"
+    export new_file="$OWRT_NAME"_"$name"_"$OWRT_DATE"."$ext"
+    cp -rf "$file" ./upload/"$new_file"
   done
 done
