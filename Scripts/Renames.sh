d2addf1655ddc245ea3098a1df566676dda9ea5b
VIKING <viking.yfy@qq.com>
2023-04-04 23:23:33 +0800

    Update Renames.sh

--- Scripts/Renames.sh
+++ Scripts/Renames.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#批量重名为 型号_日期_格式(SQUASHFS)
+#批量重名为 主机名_型号_日期
 for var in $OWRT_TYPE; do
   for file in $(find ./ -type f -iname "*$var*.*"); do
     export ext=$(basename "$file" | cut -d '.' -f 2-3)
-    export name=$(echo "$var" | tr "[:lower:]" "[:upper:]")_"$OWRT_DATE"_"SQUASHFS"
-    mv "$file" "$name.$ext"
+    export name=$(basename "$file" | cut -d '.' -f1 | grep -io "$var.*")
+    export fullname="$OWRT_NAME"_"$name"_"$OWRT_DATE"."$ext"
+    mv "$file" "$fullname"
   done
 done
