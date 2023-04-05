6a21bdc3013e2db1b41759a44fbf0819107f065f
VIKING <viking.yfy@qq.com>
2023-04-05 15:35:46 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #删除冲突主题
-rm -rf $(find ./feeds/luci/ -type d -regex ".*\(argon\|design\).*")
+rm -rf $(find ./feeds/luci/ -type d -name "*design*")
 #修改默认主机名
 sed -i "s/OpenWrt/$OWRT_NAME/g" ./package/base-files/files/bin/config_generate
 #修改默认IP地址
