6b238d18436906d65f07869bc2acb6f4aa3dcc47
VIKING <viking.yfy@qq.com>
2023-04-05 12:08:22 +0800

    Update and rename Packages.sh to Renames.sh

--- Scripts/Renames.sh
+++ Scripts/Renames.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#批量移动并重命名为 主机名_型号_日期
+#批量重命名为 主机名_型号_日期
 for var in $OWRT_TYPE; do
-  for file in $(find ./bin/targets/ -type f -iregex ".*\($var\).*\.\(bin\|iso\|vmdk\|img.gz\)"); do
+  for file in $(find ./ -type f -iname "*$var*.*"); do
     export ext=$(basename "$file" | cut -d '.' -f 2-3)
     export name=$(basename "$file" | cut -d '.' -f1 | grep -io "\($var\).*")
     export new_file="$OWRT_NAME"_"$name"_"$OWRT_DATE"."$ext"
-    cp -rf "$file" ./upload/"$new_file"
+    mv -f "$file" "$new_file"
   done
 done
