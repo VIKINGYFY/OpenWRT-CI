de865cfd8e66b7b2df489a2983f5c6e5374a007c
VIKING <viking.yfy@qq.com>
2023-04-05 09:23:43 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#批量查找对应文件，并重名为 主机名_型号_日期
+#批量移动并重命名为 主机名_型号_日期
 for var in $OWRT_TYPE; do
   for file in $(find ./bin/targets/ -type f -iregex ".*\($var\).*\.\(bin\|iso\|vmdk\|img.gz\)"); do
     export ext=$(basename "$file" | cut -d '.' -f 2-3)
