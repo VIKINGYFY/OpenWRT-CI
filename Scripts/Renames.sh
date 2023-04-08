2e3d3281a956bce8439a5e8a47c39049d92064ba
VIKING <viking.yfy@qq.com>
2023-04-08 11:30:53 +0800

    Update Renames.sh

--- Scripts/Renames.sh
+++ Scripts/Renames.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #批量重命名为 主机名_型号_日期
-for var in $OWRT_TYPE; do
-  for file in $(find ./ -type f -iname "*$var*.*" ! -iname "*.txt"); do
+for var in $OWRT_TYPE ; do
+  for file in $(find ./ -type f -iname "*$var*.*" ! -iname "*.txt") ; do
     export ext=$(basename "$file" | cut -d '.' -f 2-3)
     export name=$(basename "$file" | cut -d '.' -f 1 | grep -io "\($var\).*")
     export new_file="$OWRT_NAME"_"$name"_"$OWRT_DATE"."$ext"
