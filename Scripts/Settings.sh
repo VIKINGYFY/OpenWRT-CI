a97b1b7589e2fb738102c905094241b3ddda4438
VIKING <viking.yfy@qq.com>
2023-04-16 23:05:29 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -3,7 +3,7 @@
 #删除冲突主题
 rm -rf $(find ./feeds/luci/ -type d -regex ".*\(argon\|design\).*")
 #修改默认主机名
-sed -i "s/OpenWrt|ImmortalWrt/$OWRT_NAME/g" ./package/base-files/files/bin/config_generate
+sed -i "s/OpenWrt\|ImmortalWrt/$OWRT_NAME/g" ./package/base-files/files/bin/config_generate
 #修改默认IP地址
 sed -i "s/192.168.1.1/$OWRT_IP/g" ./package/base-files/files/bin/config_generate
 
@@ -13,7 +13,7 @@ if [[ $OWRT_URL == *"lede"* ]] ; then
   sed -i 's/luci-theme-bootstrap/luci-theme-design/g' ./feeds/luci/collections/luci/Makefile
   #修改默认时间格式
   sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
-else
+elif [[ $OWRT_URL == *"immortalwrt"* ]] ; then
   #修改默认主题为Argon
   sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 fi
