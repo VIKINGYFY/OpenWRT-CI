20ace7af68aa28df03d6e36fdae642d00682b99f
VIKING <viking.yfy@qq.com>
2023-04-16 23:04:31 +0800

    Update and rename Themes.sh to Extras.sh

--- Scripts/Extras.sh
+++ Scripts/Extras.sh
@@ -2,11 +2,11 @@
 
 #根据源码来修改
 if [[ $OWRT_URL == *"lede"* ]] ; then
-  #勾选默认主题Design
   echo "CONFIG_PACKAGE_luci-theme-design=y" >> .config
   echo "CONFIG_PACKAGE_luci-app-design-config=y" >> .config
-else
-  #勾选默认主题Argon
+elif [[ $OWRT_URL == *"immortalwrt"* ]] ; then
   echo "CONFIG_PACKAGE_luci-theme-argon=y" >> .config
   echo "CONFIG_PACKAGE_luci-app-argon-config=y" >> .config
+  echo "CONFIG_PACKAGE_luci=y" >> .config
+  echo "CONFIG_LUCI_LANG_zh_Hans=y" >> .config
 fi
