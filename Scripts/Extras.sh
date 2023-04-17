47c1d2f7e03527971045a6514f39ef467934616a
VIKING <viking.yfy@qq.com>
2023-04-17 22:36:21 +0800

    Update Extras.sh

--- Scripts/Extras.sh
+++ Scripts/Extras.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+#增加主题
+echo "CONFIG_PACKAGE_luci-theme-$OWRT_THEME=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-$OWRT_THEME-config=y" >> .config
+
 #根据源码来修改
-if [[ $OWRT_URL == *"lede"* ]] ; then
-  echo "CONFIG_PACKAGE_luci-theme-design=y" >> .config
-  echo "CONFIG_PACKAGE_luci-app-design-config=y" >> .config
-elif [[ $OWRT_URL == *"immortalwrt"* ]] ; then
-  echo "CONFIG_PACKAGE_luci-theme-argon=y" >> .config
-  echo "CONFIG_PACKAGE_luci-app-argon-config=y" >> .config
+if [[ $OWRT_URL == *"immortalwrt"* ]] ; then
+  #增加luci界面
   echo "CONFIG_PACKAGE_luci=y" >> .config
   echo "CONFIG_LUCI_LANG_zh_Hans=y" >> .config
 fi
