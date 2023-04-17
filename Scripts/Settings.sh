c9f5215037b16e8f06a0079ae4c1c6129baa9b51
VIKING <viking.yfy@qq.com>
2023-04-17 22:23:03 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#删除冲突主题
-rm -rf $(find ./feeds/luci/ -type d -regex ".*\(argon\|design\).*")
+#删除冲突插件
+rm -rf $(find ./feeds/luci/ -type d -iregex ".*\(argon\|design\|openclash\).*")
 #修改默认主机名
 sed -i "s/OpenWrt\|ImmortalWrt/$OWRT_NAME/g" ./package/base-files/files/bin/config_generate
 #修改默认IP地址
@@ -10,10 +10,10 @@ sed -i "s/192.168.1.1/$OWRT_IP/g" ./package/base-files/files/bin/config_generate
 #根据源码来修改
 if [[ $OWRT_URL == *"lede"* ]] ; then
   #修改默认主题为Design
-  sed -i 's/luci-theme-bootstrap/luci-theme-design/g' ./feeds/luci/collections/luci/Makefile
+  sed -i 's/luci-theme-bootstrap/luci-theme-design/g' $(find ./feeds/luci/collections/luci/ -type f -name "Makefile")
   #修改默认时间格式
   sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
 elif [[ $OWRT_URL == *"immortalwrt"* ]] ; then
   #修改默认主题为Argon
-  sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+  sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' $(find ./feeds/luci/collections/luci/ -type f -name "Makefile")
 fi
