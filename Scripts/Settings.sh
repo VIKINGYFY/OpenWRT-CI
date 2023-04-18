c3333f0c878e8694686eebd1d17e1fa9579d8a21
VIKING <viking.yfy@qq.com>
2023-04-18 09:08:16 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -7,7 +7,7 @@ sed -i "s/192.168.1.1/$OWRT_IP/g" ./package/base-files/files/bin/config_generate
 #修改默认主机名
 sed -i "s/OpenWrt\|ImmortalWrt/$OWRT_NAME/g" ./package/base-files/files/bin/config_generate
 #修改默认主题
-sed -i "s/luci-theme-bootstrap/luci-theme-$OWRT_THEME/g" $(find ./feeds/luci/collections/luci/ -type f -name "Makefile")
+#sed -i "s/luci-theme-bootstrap/luci-theme-$OWRT_THEME/g" $(find ./feeds/luci/collections/luci/ -type f -name "Makefile")
 
 #根据源码来修改
 if [[ $OWRT_URL == *"lede"* ]] ; then
