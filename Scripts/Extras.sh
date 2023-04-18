c093f023336697c1e5662fe9ddd5de620b589333
VIKING <viking.yfy@qq.com>
2023-04-18 18:08:07 +0800

    Update Extras.sh

--- Scripts/Extras.sh
+++ Scripts/Extras.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #增加主题
-echo "CONFIG_PACKAGE_luci-theme-$OWRT_THEME=y" >> .config
-echo "CONFIG_PACKAGE_luci-app-$OWRT_THEME-config=y" >> .config
+echo "CONFIG_PACKAGE_luci-theme-"$OWRT_THEME"=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-"$OWRT_THEME"-config=y" >> .config
 
 #根据源码来修改
 if [[ $OWRT_URL == *"immortalwrt"* ]] ; then
