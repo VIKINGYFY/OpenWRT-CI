a89ac0688a56eb50738437aa5212947ae68bab2b
VIKING <viking.yfy@qq.com>
2023-04-21 18:42:11 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -3,14 +3,14 @@
 #删除冲突插件
 rm -rf $(find ./feeds/luci/ -type d -regex ".*\(argon\|design\|openclash\).*")
 #修改默认IP地址
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/$OWRT_IP/g" ./package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/${{env.OWRT_IP}}/g" ./package/base-files/files/bin/config_generate
 #修改默认主机名
-sed -i "s/OpenWrt\|ImmortalWrt/$OWRT_NAME/g" ./package/base-files/files/bin/config_generate
+sed -i "s/OpenWrt\|ImmortalWrt/${{env.OWRT_NAME}}/g" ./package/base-files/files/bin/config_generate
 #修改默认主题
-sed -i "s/luci-theme-bootstrap/luci-theme-$OWRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
+sed -i "s/luci-theme-bootstrap/luci-theme-${{env.OWRT_THEME}}/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 
 #根据源码来修改
-if [[ $OWRT_URL == *"lede"* ]] ; then
+if [[ ${{env.OWRT_URL}} == *"lede"* ]] ; then
   #修改默认时间格式
   sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
 fi
