d3a5fa183fb681a652ed38a81d20d87a804d18df
VIKING <viking.yfy@qq.com>
2023-05-25 13:16:37 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
+#Design Theme
+git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "main" || echo "js") https://github.com/gngpp/luci-theme-design.git
+git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
+#Argon Theme
+git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-theme-argon.git
+git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-app-argon-config.git
 #Linkease
 git clone --depth=1 --single-branch https://github.com/linkease/istore.git
 git clone --depth=1 --single-branch https://github.com/linkease/nas-packages.git
 git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci.git
-#Argon Theme
-git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-theme-argon.git
-git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-app-argon-config.git
-#Design Theme
-git clone --depth=1 --single-branch https://github.com/gngpp/luci-theme-design.git
-git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
 #Open Clash
 git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/OpenClash.git
 #Pass Wall
