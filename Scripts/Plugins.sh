04d414ad8aeadbe1f7a06b840474f6996c814ffd
VIKING <viking.yfy@qq.com>
2023-10-08 22:31:23 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 #Design Theme
-git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "main" || echo "js") https://github.com/gngpp/luci-theme-design.git
+if [[ $OWRT_URL == *"lede"* ]] ; then
+  git clone --depth=1 --single-branch --branch "main" https://github.com/gngpp/luci-theme-design.git
+else
+  git clone --depth=1 --single-branch --branch "js" https://github.com/papagaye744/luci-theme-design.git
+fi
 git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
 #Argon Theme
 git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-theme-argon.git
@@ -15,6 +19,8 @@ git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/
 #Pass Wall
 git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwall.git
 git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwall-packages.git
+#Hello World
+git clone --depth=1 --single-branch https://github.com/fw876/helloworld.git
 
 #预置OpenClash内核和GEO数据
 export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
