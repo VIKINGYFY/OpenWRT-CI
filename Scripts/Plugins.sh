7a05a3a6d709a7ac057e63e658e24cae362b4b64
VIKING <viking.yfy@qq.com>
2023-10-17 18:06:41 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -35,12 +35,14 @@ export TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
 export GEO_MMDB=https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb
 export GEO_SITE=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat
 export GEO_IP=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat
+export META_DB=https://github.com/MetaCubeX/meta-rules-dat/raw/release/geoip.metadb
 
 cd ./OpenClash/luci-app-openclash/root/etc/openclash
 
 curl -sfL -o ./Country.mmdb $GEO_MMDB
 curl -sfL -o ./GeoSite.dat $GEO_SITE
 curl -sfL -o ./GeoIP.dat $GEO_IP
+curl -sfL -o ./GeoIP.metadb $META_DB
 
 mkdir ./core && cd ./core
 
