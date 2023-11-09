e12b3b42153c36b8b3125e37059ad767ad47bcc5
VIKING <viking.yfy@qq.com>
2023-11-09 08:30:59 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 #Design Theme
-if [[ $OWRT_URL == *"lede"* ]] ; then
-  git clone --depth=1 --single-branch --branch "main" https://github.com/gngpp/luci-theme-design.git
-else
-  git clone --depth=1 --single-branch --branch "js" https://github.com/papagaye744/luci-theme-design.git
-fi
+git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "main" || echo "js") https://github.com/gngpp/luci-theme-design.git
 git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
 #Argon Theme
 git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-theme-argon.git
