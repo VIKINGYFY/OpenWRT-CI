6396b03280121ee5bf93909861da04136c606ef2
coolbele <64392071+coolbele@users.noreply.github.com>
2023-11-30 09:08:17 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -2,10 +2,8 @@
 
 #删除冲突插件
 rm -rf $(find ./feeds/luci/ -type d -iregex ".*\(argon\|design\|helloworld\|homeproxy\|openclash\|passwall\).*")
-#修改默认主题
-sed -i "s/luci-theme-bootstrap/luci-theme-$OWRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 #修改默认IP地址
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/$OWRT_IP/g" ./package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.10.1/g' ./package/base-files/files/bin/config_generate
 #修改默认主机名
 sed -i "s/hostname='.*'/hostname='$OWRT_NAME'/g" ./package/base-files/files/bin/config_generate
 #修改默认时区
