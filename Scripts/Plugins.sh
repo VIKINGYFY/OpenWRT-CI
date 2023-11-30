8bb8813855d3e5393455b2974420831d978e1d62
VIKING <viking.yfy@qq.com>
2023-11-30 20:32:49 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -3,14 +3,18 @@
 #精简git命令
 CLONE="git clone --depth=1 --single-branch"
 
+#MOS DNS
+$CLONE https://github.com/sbwml/luci-app-mosdns.git
 #Tiny Filemanager
 $CLONE https://github.com/muink/luci-app-tinyfilemanager.git
+
 #Design Theme
 $CLONE --branch $(echo $OWRT_URL | grep -iq "lede" && echo "main" || echo "js") https://github.com/gngpp/luci-theme-design.git
 $CLONE https://github.com/gngpp/luci-app-design-config.git
 #Argon Theme
 $CLONE --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-theme-argon.git
 $CLONE --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-app-argon-config.git
+
 #Pass Wall
 $CLONE https://github.com/xiaorouji/openwrt-passwall.git
 $CLONE https://github.com/xiaorouji/openwrt-passwall2.git
@@ -23,7 +27,7 @@ if [[ $OWRT_URL == *"lede"* ]] ; then
 fi
 #Home Proxy
 if [[ $OWRT_URL == *"immortalwrt"* ]] ; then
-  $CLONE --branch "dev" https://github.com/immortalwrt/homeproxy.git
+  $CLONE --branch "master" https://github.com/immortalwrt/homeproxy.git
 fi
 
 #修改Tiny Filemanager汉化
