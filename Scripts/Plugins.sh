8a4988e6bcf3b2e6a89818e30b3f4b4e25094d99
VIKING <viking.yfy@qq.com>
2023-11-30 16:53:54 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -24,7 +24,7 @@ fi
 #Home Proxy
 if [[ $OWRT_URL == *"immortalwrt"* ]] ; then
   #$CLONE --branch "dev" https://github.com/immortalwrt/homeproxy.git
-  $CLONE --branch "dev" https://github.com/douglarek/luci-app-homeproxy.git
+  $CLONE --branch "mdev" https://github.com/muink/homeproxy.git
 fi
 
 #修改Tiny Filemanager汉化
