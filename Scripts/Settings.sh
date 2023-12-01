0eefbfb19633a187f6361805615a34716f4ed8e3
VIKING <viking.yfy@qq.com>
2023-12-01 23:20:12 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #删除冲突插件
-rm -rf $(find ./feeds/luci/ -maxdepth 2 -type d -iregex ".*\(argon\|design\|helloworld\|homeproxy\|openclash\|passwall\|mosdns\).*")
+rm -rf $(find ./feeds/luci/ -maxdepth 2 -type d -iregex ".*\(argon\|design\|helloworld\|homeproxy\|openclash\|passwall\).*")
 #修改默认主题
 sed -i "s/luci-theme-bootstrap/luci-theme-$OWRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 #修改默认IP地址
@@ -11,22 +11,18 @@ sed -i "s/hostname='.*'/hostname='$OWRT_NAME'/g" ./package/base-files/files/bin/
 #修改默认时区
 sed -i "s/timezone='.*'/timezone='CST-8'/g" ./package/base-files/files/bin/config_generate
 sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
-
-#根据源码来修改
-if [[ $OWRT_URL == *"lede"* ]] ; then
-	#修改默认时间格式
-	sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
-fi
+#修改默认时间格式
+[[ $OWRT_URL == *"lede"* ]] && sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
 
 #自定义更新软件包版本
 UPDATE_VERSION() {
 	local pkg_name=$1
 	local new_ver=$2
 	local new_hash=$3
-	local pkg_file=$(find ./feeds/packages/*/$pkg_name -name "Makefile" 2>/dev/null)
+	local pkg_file=$(find ./feeds/packages/*/$pkg_name/ -type f -name "Makefile" 2>/dev/null)
 
 	if [ -f "$pkg_file" ]; then
-		local old_ver=$(grep -oP 'PKG_VERSION:=\K.*' "$pkg_file")
+		local old_ver=$(egrep -o 'PKG_VERSION:=\K.*' "$pkg_file")
 		if dpkg --compare-versions "$old_ver" lt "$new_ver"; then
 			sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=$new_ver/g" "$pkg_file"
 			sed -i "s/PKG_HASH:=.*/PKG_HASH:=$new_hash/g" "$pkg_file"
