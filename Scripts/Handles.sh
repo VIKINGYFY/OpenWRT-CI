3fef3990bc74f71d53d6ca2e3e21b56d846e8626
VIKING <viking.yfy@qq.com>
2023-12-03 00:47:48 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -7,9 +7,9 @@ if [[ -d "homeproxy" ]]; then
 	UPDATE_RESOURCES() {
 		local res_type=$1
 		local res_repo=$2
-		local res_depth=$3
-		local res_branch=$4
-		local res_file=$5
+		local res_branch=$3
+		local res_file=$4
+		local res_depth=${5:-1}
 
 		git clone --depth=$res_depth --single-branch --branch $res_branch "https://github.com/$res_repo.git" ./$res_type/
 
@@ -18,13 +18,11 @@ if [[ -d "homeproxy" ]]; then
 		if [[ "${res_file##*.}" == "txt" ]]; then
 			echo $(git log -1 --pretty=format:'%s' -- $res_file | grep -o "[0-9]*") > "$res_type.ver"
 			mv -f $res_file "$res_type.${res_file##*.}"
-		fi
-		if [[ "${res_file##*.}" == "zip" ]]; then
+		elif [[ "${res_file##*.}" == "zip" ]]; then
 			echo $(git log -1 --pretty=format:'%s' | cut -d ' ' -f 1) > "$res_type.ver"
 			curl -sfL -O "https://github.com/$res_repo/archive/$res_file"
 			mv -f $res_file $HP_PATCH/"${res_repo//\//_}.${res_file##*.}"
-		fi
-		if [[ "${res_file##*.}" == "db" ]]; then
+		elif [[ "${res_file##*.}" == "db" ]]; then
 			local res_ver=$(git tag | tail -n 1)
 			echo $res_ver > "$res_type.ver"
 			curl -sfL -O "https://github.com/$res_repo/releases/download/$res_ver/$res_file"
@@ -35,13 +33,13 @@ if [[ -d "homeproxy" ]]; then
 		cd .. && rm -rf ./$res_type/
 	}
 
-	UPDATE_RESOURCES "china_ip4" "1715173329/IPCIDR-CHINA" "5" "master" "ipv4.txt"
-	UPDATE_RESOURCES "china_ip6" "1715173329/IPCIDR-CHINA" "5" "master" "ipv6.txt"
-	UPDATE_RESOURCES "gfw_list" "Loyalsoldier/v2ray-rules-dat" "1" "release" "gfw.txt"
-	UPDATE_RESOURCES "china_list" "Loyalsoldier/v2ray-rules-dat" "1" "release" "direct-list.txt"
-	UPDATE_RESOURCES "geoip" "1715173329/sing-geoip" "1" "master" "geoip.db"
-	UPDATE_RESOURCES "geosite" "1715173329/sing-geosite" "1" "master" "geosite.db"
-	UPDATE_RESOURCES "clash_dashboard" "MetaCubeX/metacubexd" "1" "gh-pages" "gh-pages.zip"
+	UPDATE_RESOURCES "china_ip4" "1715173329/IPCIDR-CHINA" "master" "ipv4.txt" "5"
+	UPDATE_RESOURCES "china_ip6" "1715173329/IPCIDR-CHINA" "master" "ipv6.txt" "5"
+	UPDATE_RESOURCES "gfw_list" "Loyalsoldier/v2ray-rules-dat" "release" "gfw.txt"
+	UPDATE_RESOURCES "china_list" "Loyalsoldier/v2ray-rules-dat" "release" "direct-list.txt"
+	UPDATE_RESOURCES "geoip" "1715173329/sing-geoip" "master" "geoip.db"
+	UPDATE_RESOURCES "geosite" "1715173329/sing-geosite" "master" "geosite.db"
+	UPDATE_RESOURCES "clash_dashboard" "MetaCubeX/metacubexd" "gh-pages" "gh-pages.zip"
 fi
 
 #预置OpenClash内核和数据
