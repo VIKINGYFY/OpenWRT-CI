76c9393ae1037216f1ec02601ed875097e08a428
VIKING <viking.yfy@qq.com>
2023-12-08 14:46:51 +0800

    更新 Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -57,12 +57,12 @@ fi
 #预置OpenClash内核和数据
 if [ -d *"OpenClash"* ]; then
 	CORE_VER="https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version"
-	CORE_TUN="https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux"
-	CORE_DEV="https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux"
-	CORE_MATE="https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux"
-
 	CORE_TYPE=$(echo $WRT_TARGET | egrep -iq "64|86" && echo "amd64" || echo "arm64")
-	TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
+	CORE_TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
+
+	CORE_DEV="https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux-$CORE_TYPE.tar.gz"
+	CORE_MATE="https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux-$CORE_TYPE.tar.gz"
+	CORE_TUN="https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux-$CORE_TYPE-$CORE_TUN_VER.gz"
 
 	GEO_MMDB="https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb"
 	GEO_SITE="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat"
@@ -78,17 +78,11 @@ if [ -d *"OpenClash"* ]; then
 
 	mkdir ./core/ && cd ./core/
 
-	curl -sfL -o tun.gz "$CORE_TUN"-"$CORE_TYPE"-"$TUN_VER".gz
-	gzip -d tun.gz && mv -f tun clash_tun
+	curl -sfL -o meta.tar.gz $CORE_MATE && tar -zxf meta.tar.gz && mv -f clash clash_meta
+	curl -sfL -o tun.gz $CORE_TUN && gzip -d tun.gz && mv -f tun clash_tun
+	curl -sfL -o dev.tar.gz $CORE_DEV && tar -zxf dev.tar.gz
 
-	curl -sfL -o meta.tar.gz "$CORE_MATE"-"$CORE_TYPE".tar.gz
-	tar -zxf meta.tar.gz && mv -f clash clash_meta
-
-	curl -sfL -o dev.tar.gz "$CORE_DEV"-"$CORE_TYPE".tar.gz
-	tar -zxf dev.tar.gz
-
-	chmod +x clash*
-	rm -rf *.gz
+	chmod +x ./clash* && rm -rf ./*.gz
 
 	echo "openclash date has been updated!"
 fi
