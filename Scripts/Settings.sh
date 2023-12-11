3e83c28649ebb98460cda82b42ee4595c148cd2b
VIKING <viking.yfy@qq.com>
2023-12-11 23:47:54 +0800

    更新 Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -2,6 +2,8 @@
 
 #修改默认主题
 sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
+#添加编译日期标识
+sed -i "s/DISTRIB_TARGET='\(.*\)'/DISTRIB_TARGET='\1 $WRT_REPO-$WRT_DATE'/g" ./package/base-files/files/etc/openwrt_release
 
 CFG_FILE="./package/base-files/files/bin/config_generate"
 #修改默认IP地址
@@ -13,15 +15,8 @@ sed -i "s/timezone='.*'/timezone='CST-8'/g" $CFG_FILE
 sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" $CFG_FILE
 
 if [[ $WRT_URL == *"lede"* ]]; then
-	LEDE_FILE=$(find ./package/lean/autocore/files/ -type f -name "index.htm")
 	#修改默认时间格式
-	sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $LEDE_FILE
-	#添加编译日期标识
-	sed -i "s/(\(<%=pcdata(ver.luciversion)%>\))/\1 \/ $WRT_REPO-$WRT_DATE/" $LEDE_FILE
-elif [[ $WRT_URL == *"immortalwrt"* ]] ; then
-	#添加编译日期标识
-	VER_FILE=$(find ./feeds/luci/modules/ -type f -name "10_system.js")
-	awk -v wrt_repo="$WRT_REPO" -v wrt_date="$WRT_DATE" '{ gsub(/(\(luciversion \|\| \047\047\))/, "& + (\047 / "wrt_repo"-"wrt_date"\047)") } 1' $VER_FILE > temp.js && mv -f temp.js $VER_FILE
+	sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/lean/autocore/files/ -type f -name "index.htm")
 fi
 
 #配置文件修改
