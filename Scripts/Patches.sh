fce0158c502238068b150b7c90b8d4074aa2ceed
VIKING <viking.yfy@qq.com>
2024-01-21 11:36:31 +0800

    新增 MTK系列补丁

---
+++ Scripts/Patches.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#加回部分设备支持
+if [[ $WRT_URL == *"immortalwrt"* && $WRT_TARGET == "Mediatek" ]]; then
+	cp -rf ./Patches/mediatek/*.dts ./wrt/target/linux/mediatek/dts/
+	patch -R -p1 -d ./wrt < ./Patches/mediatek/*.patch
+fi
