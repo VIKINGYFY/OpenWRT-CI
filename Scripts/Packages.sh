183a1a9f0881810a4a59bdc9a36810dbcf96a2f9
VIKING <viking.yfy@qq.com>
2024-02-21 13:46:35 +0800

    移除不常用插件

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -20,8 +20,6 @@ UPDATE_PACKAGE() {
 	fi
 }
 
-UPDATE_PACKAGE "tinyfilemanager" "muink/luci-app-tinyfilemanager" "master"
-
 UPDATE_PACKAGE "design" "gngpp/luci-theme-design" "$([[ $WRT_URL == *"lede"* ]] && echo "main" || echo "js")"
 UPDATE_PACKAGE "design-config" "gngpp/luci-app-design-config" "master"
 UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "$([[ $WRT_URL == *"lede"* ]] && echo "18.06" || echo "master")"
