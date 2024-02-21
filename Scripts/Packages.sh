c4b84a9d8d9cc5b1736b0845e06363a2c8da4572
VIKING <viking.yfy@qq.com>
2024-02-21 15:04:21 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -29,8 +29,8 @@ UPDATE_PACKAGE "helloworld" "fw876/helloworld" "master"
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev"
 
 if [[ $WRT_URL == *"immortalwrt"* ]]; then
-	UPDATE_PACKAGE "homeproxy" "muink/homeproxy" "dev"
-	#UPDATE_PACKAGE "homeproxy" "immortalwrt/homeproxy" "dev"
+	#UPDATE_PACKAGE "homeproxy" "muink/homeproxy" "dev"
+	UPDATE_PACKAGE "homeproxy" "immortalwrt/homeproxy" "dev"
 fi
 
 #更新软件包版本
