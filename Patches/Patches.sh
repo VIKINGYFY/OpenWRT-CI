d7c4c704940dedb649473da951d26c35a2e7c629
VIKING <viking.yfy@qq.com>
2024-05-11 16:11:18 +0800

    调整补丁目录

--- Patches/Patches.sh
+++ Patches/Patches.sh
@@ -5,5 +5,5 @@ if [[ $WRT_URL == *"immortalwrt"* && $WRT_TARGET == "Mediatek" ]]; then
 	cp -rf ./Patches/mediatek/*.dts ./wrt/target/linux/mediatek/dts/
 	patch -R -p1 -d ./wrt < ./Patches/mediatek/*.patch
 
-	echo "MTK patch has been installed!"
+	echo "$WRT_TARGET patch has been installed!"
 fi
