d3b73a723c7e1737d963ba4c264c5547deaac011
VIKING <viking.yfy@qq.com>
2024-06-07 23:34:29 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -22,7 +22,7 @@ if [[ $WRT_URL == *"lede"* ]]; then
 	sed -i "s/ssid=.*/ssid=$WRT_WIFI/g" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
 elif [[ $WRT_URL == *"immortalwrt"* ]]; then
 	#添加编译日期标识
-	VER_FILE=$(find ./feeds/luci/modules/ -type f -name "10_system.js")
+	VER_FILE=$(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
 	awk -v wrt_repo="$WRT_REPO" -v wrt_date="$WRT_DATE" '{ gsub(/(\(luciversion \|\| \047\047\))/, "& + (\047 / "wrt_repo"-"wrt_date"\047)") } 1' $VER_FILE > temp.js && mv -f temp.js $VER_FILE
 	#修改默认WIFI名
 	sed -i "s/ssid=.*/ssid=$WRT_WIFI/g" ./package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
