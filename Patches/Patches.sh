9c284c0dcf0d031f0f8a9d1840428e87f5f40d07
VIKING <viking.yfy@qq.com>
2024-06-13 23:55:45 +0800

    新增亚瑟NSS支持

--- Patches/Patches.sh
+++ Patches/Patches.sh
@@ -2,14 +2,19 @@
 
 #加回部分设备支持
 if [[ $WRT_URL == *"immortalwrt"* && $WRT_TARGET == "Mediatek" ]]; then
-	cp -rf ./Patches/mediatek/*.dts ./wrt/target/linux/mediatek/dts/
-	patch -R -p1 -d ./wrt < ./Patches/mediatek/*.patch
+	mv -f ./Patches/mediatek/*.dts ./wrt/target/linux/mediatek/dts/
+
+	patch -R -p1 -d ./wrt < ./Patches/mediatek/mtk*.patch
 
 	echo "$WRT_TARGET patch has been installed!"
 fi
 
 if [[ $WRT_URL == *"lede"* && $WRT_TARGET == "Qualcom" ]]; then
-	cd ./wrt/ && git apply ../Patches/qualcom/*.patch
+	mv -f ./Patches/qualcom/*.dts ./wrt/target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/
+	mv -f ./Patches/qualcom/*.ipq6018 ./wrt/package/firmware/ipq-wifi/src/
+	mv -f ./Patches/qualcom/0920*.patch ./wrt/target/linux/qualcommax/patches-6.1/
+
+	patch -R -p1 -d ./wrt < ./Patches/qualcom/qcam*.patch
 
 	echo "$WRT_TARGET patch has been installed!"
 fi
