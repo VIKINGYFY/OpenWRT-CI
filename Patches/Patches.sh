f87436f0cc46b10e8478cc955e5df377235254be
VIKING <viking.yfy@qq.com>
2024-06-18 18:57:09 +0800

    移除LEDE源补丁

--- Patches/Patches.sh
+++ Patches/Patches.sh
@@ -7,13 +7,3 @@ if [[ $WRT_URL == *"immortalwrt"* && $WRT_TARGET == "Mediatek" ]]; then
 
 	echo "$WRT_TARGET patch has been installed!"
 fi
-
-if [[ $WRT_URL == *"lede"* && $WRT_TARGET == "Qualcom" ]]; then
-	mv -f ./Patches/qualcom/*.dts ./wrt/target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/
-	mv -f ./Patches/qualcom/*.ipq6018 ./wrt/package/firmware/ipq-wifi/src/
-	mv -f ./Patches/qualcom/0920*.patch ./wrt/target/linux/qualcommax/patches-6.1/
-
-	patch -R -p1 -d ./wrt < ./Patches/qualcom/qcam*.patch
-
-	echo "$WRT_TARGET patch has been installed!"
-fi
