297de504ac4fc4f2eff50f285c2f1fe728fc2528
VIKING <viking.yfy@qq.com>
2024-06-25 00:20:28 +0800

    Revert "切换nss版本"
    This reverts commit 606788dc0d128d3bdf45c23d81c0daa5f591c6df.

--- Patches/Patches.sh
+++ Patches/Patches.sh
@@ -7,9 +7,3 @@ if [[ $WRT_URL == *"immortalwrt"* && $WRT_TARGET == "Mediatek" ]]; then
 
 	echo "$WRT_TARGET patch has been installed!"
 fi
-
-if [[ $WRT_URL == *"openwrt-6.x"* && $WRT_TARGET == "Qualcom" ]]; then
-	sed -i 's/nss-packages.git/nss-packages.git;NSS-12.4-K6.x/g' ./wrt/feeds.conf.default
-
-	echo "$WRT_TARGET patch has been installed!"
-fi
