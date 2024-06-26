54b608da3685d8494455afc424775ef24a4edc04
VIKING <viking.yfy@qq.com>
2024-06-26 17:44:03 +0800

    修复报错

--- Patches/Patches.sh
+++ Patches/Patches.sh
@@ -8,7 +8,7 @@ if [[ $WRT_URL == *"immortalwrt"* && $WRT_TARGET == "Mediatek" ]]; then
 fi
 
 if [[ $WRT_URL == *"lede"* ]]; then
-	sed -i "|\#src-git|d" ./wrt/feeds.conf.default
+	sed -i "/#src/d" ./wrt/feeds.conf.default
 	sed -i "s|\(src-git luci\).*|\1 https://github.com/coolsnowwolf/luci.git;openwrt-23.05|g" ./wrt/feeds.conf.default
 
 	echo "$WRT_URL patch has been installed!"
