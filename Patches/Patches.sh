e326c5eb2917c4ef5dcffeab08e45a8729bf96a7
VIKING <viking.yfy@qq.com>
2024-06-26 16:08:13 +0800

    Update Patches.sh

--- Patches/Patches.sh
+++ Patches/Patches.sh
@@ -2,8 +2,14 @@
 
 if [[ $WRT_URL == *"immortalwrt"* && $WRT_TARGET == "Mediatek" ]]; then
 	mv -f ./Patches/mediatek/*.dts ./wrt/target/linux/mediatek/dts/
-
 	patch -R -p1 -d ./wrt < ./Patches/mediatek/mtk*.patch
 
 	echo "$WRT_TARGET patch has been installed!"
+fi
+
+if [[ $WRT_URL == *"lede"* ]]; then
+	sed -i "|\#src-git|d" ./wrt/feeds.conf.default
+	sed -i "s|\(src-git luci\).*|\1 https://github.com/coolsnowwolf/luci.git;openwrt-23.05|g" ./wrt/feeds.conf.default
+
+	echo "$WRT_URL patch has been installed!"
 fi
