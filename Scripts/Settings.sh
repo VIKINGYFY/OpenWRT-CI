7f540e85c37674457010d88c62b6dcb40ce9d33b
liaoke123 <104013620+liaoke01@users.noreply.github.com>
2024-07-04 10:24:14 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -47,5 +47,5 @@ if [[ $WRT_URL == *"lede"* ]]; then
 else
 	echo "CONFIG_PACKAGE_luci=y" >> ./.config
 	echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
+	#echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
 fi
