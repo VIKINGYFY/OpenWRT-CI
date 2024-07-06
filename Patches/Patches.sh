e9804ab85ec7825b50d5980006772ab6b0e4577e
VIKING <viking.yfy@qq.com>
2024-07-06 16:45:47 +0800

    Revert "移除MTK补丁"
    This reverts commit 16e8768ccd138c3491d72233c6cde170fd3932d9.

--- Patches/Patches.sh
+++ Patches/Patches.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [[ $WRT_URL == *"immortalwrt"* && $WRT_TARGET == "Mediatek" ]]; then
+	mv -f ./Patches/mediatek/*.dts ./wrt/target/linux/mediatek/dts/
+	patch -R -p1 -d ./wrt < ./Patches/mediatek/mtk*.patch
+
+	echo "$WRT_TARGET patch has been installed!"
+fi
+
 if [[ $WRT_URL == *"lede"* ]]; then
 	sed -i "/#src/d" ./wrt/feeds.conf.default
 	sed -i "s|\(src-git luci\).*|\1 https://github.com/coolsnowwolf/luci.git;openwrt-23.05|g" ./wrt/feeds.conf.default
