0302ae80c46bac4c0c7b8cc0f3f6c40b549f261e
VIKING <viking.yfy@qq.com>
2024-08-13 18:22:35 +0800

    新增alpha主题和wolplus插件

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -32,11 +32,15 @@ UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
 UPDATE_PACKAGE "advancedplus" "VIKINGYFY/luci-app-advancedplus" "main"
 UPDATE_PACKAGE "gecoosac" "lwb1978/openwrt-gecoosac" "main"
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
+UPDATE_PACKAGE "luci-app-wolplus" "animegasan/luci-app-wolplus" "main"
 
 if [[ $WRT_REPO != *"lede"* ]]; then
 	UPDATE_PACKAGE "daed" "QiuSimons/luci-app-daed" "master"
 	UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
 	UPDATE_PACKAGE "mihomo" "morytyann/OpenWrt-mihomo" "main" "pkg"
+
+	UPDATE_PACKAGE "alpha" "derisamedia/luci-theme-alpha" "master"
+	UPDATE_PACKAGE "alpha-config" "animegasan/luci-app-alpha-config" "master"
 fi
 
 if [[ $WRT_REPO == *"openwrt-6.x"* ]]; then
