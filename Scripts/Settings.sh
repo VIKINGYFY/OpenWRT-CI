58f1c1e1776ec6da59ae87c3a4f187be91e95cf5
VIKING <viking.yfy@qq.com>
2024-12-26 16:37:44 +0800

    组件调整

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -47,4 +47,7 @@ if [[ $WRT_TARGET == *"IPQ"* ]]; then
 	#取消nss相关feed
 	echo "CONFIG_FEED_nss_packages=n" >> ./.config
 	echo "CONFIG_FEED_sqm_scripts_nss=n" >> ./.config
+	#设置NSS版本
+	echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=n" >> ./.config
+	echo "CONFIG_NSS_FIRMWARE_VERSION_12_2=y" >> ./.config
 fi
