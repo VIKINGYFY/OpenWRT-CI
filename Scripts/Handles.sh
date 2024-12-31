09bc12e152821371e92a02b3b9e8de3da8eb46dc
VIKING <viking.yfy@qq.com>
2024-12-31 13:57:39 +0800

    项目调整

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-PKG_PATCH="$GITHUB_WORKSPACE/wrt/package/"
+PKG_PATH="$GITHUB_WORKSPACE/wrt/package/"
 
 #预置HomeProxy数据
 if [ -d *"homeproxy"* ]; then
-	HP_RULES="surge"
-	HP_PATCH="homeproxy/root/etc/homeproxy"
+	HP_RULE="surge"
+	HP_PATH="homeproxy/root/etc/homeproxy"
 
-	rm -rf ./$HP_PATCH/resources/*
+	rm -rf ./$HP_PATH/resources/*
 
-	git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" ./$HP_RULES/
-	cd ./$HP_RULES/ && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
+	git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" ./$HP_RULE/
+	cd ./$HP_RULE/ && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
 
 	echo $RES_VER | tee china_ip4.ver china_ip6.ver china_list.ver gfw_list.ver
 	awk -F, '/^IP-CIDR,/{print $2 > "china_ip4.txt"} /^IP-CIDR6,/{print $2 > "china_ip6.txt"}' cncidr.txt
 	sed 's/^\.//g' direct.txt > china_list.txt ; sed 's/^\.//g' gfw.txt > gfw_list.txt
-	mv -f ./{china_*,gfw_list}.{ver,txt} ../$HP_PATCH/resources/
+	mv -f ./{china_*,gfw_list}.{ver,txt} ../$HP_PATH/resources/
 
-	cd .. && rm -rf ./$HP_RULES/
+	cd .. && rm -rf ./$HP_RULE/
 
-	cd $PKG_PATCH && echo "homeproxy date has been updated!"
+	cd $PKG_PATH && echo "homeproxy date has been updated!"
 fi
 
 #移除Shadowsocks组件
@@ -29,7 +29,7 @@ if [ -f "$PW_FILE" ]; then
 	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_ShadowsocksR/,/default n/d' $PW_FILE
 	sed -i '/Shadowsocks_NONE/d; /Shadowsocks_Libev/d; /ShadowsocksR/d' $PW_FILE
 
-	cd $PKG_PATCH && echo "passwall has been fixed!"
+	cd $PKG_PATH && echo "passwall has been fixed!"
 fi
 
 SP_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-ssr-plus/Makefile")
@@ -38,7 +38,7 @@ if [ -f "$SP_FILE" ]; then
 	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_ShadowsocksR/,/x86_64/d' $SP_FILE
 	sed -i '/Shadowsocks_NONE/d; /Shadowsocks_Libev/d; /ShadowsocksR/d' $SP_FILE
 
-	cd $PKG_PATCH && echo "ssr-plus has been fixed!"
+	cd $PKG_PATH && echo "ssr-plus has been fixed!"
 fi
 
 #修复TailScale配置文件冲突
@@ -46,5 +46,5 @@ TS_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/tailscale/Ma
 if [ -f "$TS_FILE" ]; then
 	sed -i '/\/files/d' $TS_FILE
 
-	cd $PKG_PATCH && echo "tailscale has been fixed!"
+	cd $PKG_PATH && echo "tailscale has been fixed!"
 fi
