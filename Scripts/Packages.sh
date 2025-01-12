35da3e05ae7c4c290ac8aa0f4fd726fd30b109bc
VIKING <viking.yfy@qq.com>
2025-01-12 14:04:45 +0800

    项目调整

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -13,7 +13,7 @@ UPDATE_PACKAGE() {
 	git clone --depth=1 --single-branch --branch $PKG_BRANCH "https://github.com/$PKG_REPO.git"
 
 	if [[ $PKG_SPECIAL == "pkg" ]]; then
-		find ./$REPO_NAME/*/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -prune | xargs -I {} cp -rf {} ./
+		find ./$REPO_NAME/*/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -prune -exec cp -rf {} ./ \;
 		rm -rf ./$REPO_NAME/
 	elif [[ $PKG_SPECIAL == "name" ]]; then
 		mv -f $REPO_NAME $PKG_NAME
