3e63cf5b0d02e2db462d6b84df2ae4369182c3bc
Yon8 <102728669+Yon8@users.noreply.github.com>
2025-01-15 15:45:33 +0800

    To compile the luci-app-mosdns APK

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -31,6 +31,7 @@ UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
 
 UPDATE_PACKAGE "alist" "sbwml/luci-app-alist" "main"
 UPDATE_PACKAGE "mosdns" "sbwml/luci-app-mosdns" "v5"
+UPDATE_PACKAGE "luci-app-mosdns" "sbwml/luci-app-mosdns" "v5"
 UPDATE_PACKAGE "wol" "VIKINGYFY/packages" "main" "pkg"
 
 UPDATE_PACKAGE "luci-app-gecoosac" "lwb1978/openwrt-gecoosac" "main"
