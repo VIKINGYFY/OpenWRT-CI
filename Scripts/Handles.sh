a90278eb74f05570913597153cd94f10478d0589
VIKING <viking.yfy@qq.com>
2025-03-20 14:25:41 +0800

    项目调整

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -26,8 +26,8 @@ fi
 if [ -d *"luci-theme-argon"* ]; then
 	cd ./luci-theme-argon/
 
-	sed -i '/font-weight:/ {/!important/! s/\(font-weight:\s*\)[^;]*;/\1normal;/}' $(find ./luci-theme-argon -type f -iname "*.css")
-	sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/" ./luci-app-argon-config/root/etc/config/argon
+	sed -i "/font-weight:/ { /important/! { /\/\*/! s/:.*/: var(--font-weight);/ } }" $(find ./luci-theme-argon -type f -iname "*.css")
+	sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" ./luci-app-argon-config/root/etc/config/argon
 
 	cd $PKG_PATH && echo "theme-argon has been fixed!"
 fi
--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -6,32 +6,25 @@ UPDATE_PACKAGE() {
 	local PKG_REPO=$2
 	local PKG_BRANCH=$3
 	local PKG_SPECIAL=$4
-	local CUSTOM_NAMES=($5)  # 第5个参数为自定义名称列表
-	local REPO_NAME=$(echo $PKG_REPO | cut -d '/' -f 2)
+	local PKG_LIST=("$PKG_NAME" $5)  # 第5个参数为自定义名称列表
+	local REPO_NAME=${PKG_REPO#*/}
 
 	echo " "
 
-	# 将 PKG_NAME 加入到需要查找的名称列表中
-	if [ ${#CUSTOM_NAMES[@]} -gt 0 ]; then
-		CUSTOM_NAMES=("$PKG_NAME" "${CUSTOM_NAMES[@]}")  # 将 PKG_NAME 添加到自定义名称列表的开头
-	else
-		CUSTOM_NAMES=("$PKG_NAME")  # 如果没有自定义名称，则只使用 PKG_NAME
-	fi
-
 	# 删除本地可能存在的不同名称的软件包
-	for NAME in "${CUSTOM_NAMES[@]}"; do
+	for NAME in "${PKG_LIST[@]}"; do
 		# 查找匹配的目录
-		echo "Searching directory: $NAME"
+		echo "Search directory: $NAME"
 		local FOUND_DIRS=$(find ../feeds/luci/ ../feeds/packages/ -maxdepth 3 -type d -iname "*$NAME*" 2>/dev/null)
 
 		# 删除找到的目录
 		if [ -n "$FOUND_DIRS" ]; then
-			echo "$FOUND_DIRS" | while read -r DIR; do
+			while read -r DIR; do
 				rm -rf "$DIR"
-				echo "Deleted directory: $DIR"
-			done
+				echo "Delete directory: $DIR"
+			done <<< "$FOUND_DIRS"
 		else
-			echo "No directories found matching name: $NAME"
+			echo "Not fonud directory: $NAME"
 		fi
 	done
 
