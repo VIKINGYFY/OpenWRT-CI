12f47b4ceaadb8826dd29fccdba681fdb6bb843b
xiahfei <124034214+xiahfei@users.noreply.github.com>
2025-05-21 15:15:12 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -46,7 +46,7 @@ UPDATE_PACKAGE() {
 
 # UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
 #UPDATE_PACKAGE "argon" "sbwml/luci-theme-argon" "openwrt-24.10"
-UPDATE_PACKAGE "linkease" "/linkease/nas-packages-luci/tree/main/luci/luci-app-linkease"
+UPDATE_PACKAGE "linkease" "/linkease/nas-packages-luci/tree/main/luci/luci-app-linkease" "main"
 #UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "js"
 UPDATE_PACKAGE "design" "/0x676e67/luci-theme-design" "js"
 #UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
