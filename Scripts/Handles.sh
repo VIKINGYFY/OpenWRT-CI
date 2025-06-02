f12a398f81d405078283fb35b03a1afbd0672186
VIKING <viking.yfy@qq.com>
2025-06-03 00:26:09 +0800

    项目调整

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -65,3 +65,13 @@ if [ -f "$TS_FILE" ]; then
 
 	cd $PKG_PATH && echo "tailscale has been fixed!"
 fi
+
+#修复Rust编译失败
+RUST_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/rust/Makefile")
+if [ -f "$RUST_FILE" ]; then
+	echo " "
+
+	sed -i 's/ci-llvm=true/ci-llvm=false/g' $RUST_FILE
+
+	cd $PKG_PATH && echo "rust has been fixed!"
+fi
