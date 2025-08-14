469c2233ebe1d01e56622f6a87167011798a4267
Yihuki <wangbovbvb@gmail.com>
2025-08-14 12:55:16 +0800

    Use pre-release software version

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -115,5 +115,5 @@ UPDATE_VERSION() {
 }
 
 #UPDATE_VERSION "软件包名" "测试版，true，可选，默认为否"
-UPDATE_VERSION "sing-box"
-#UPDATE_VERSION "tailscale"
+UPDATE_VERSION "sing-box" "true"
+UPDATE_VERSION "tailscale"
