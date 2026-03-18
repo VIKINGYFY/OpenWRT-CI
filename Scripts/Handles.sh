1e17237d8e02abb4175a2fbae093634f9d2c3474
feeds168 <feeds168@gmail.com>
2026-03-18 17:27:29 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -25,15 +25,15 @@ if [ -d *"homeproxy"* ]; then
 fi
 
 #修改argon主题字体和颜色
-if [ -d *"luci-theme-argon"* ]; then
-	echo " "
-
-	cd ./luci-theme-argon/
-
-	sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" ./luci-app-argon-config/root/etc/config/argon
-
-	cd $PKG_PATH && echo "theme-argon has been fixed!"
-fi
+#if [ -d *"luci-theme-argon"* ]; then
+#	echo " "
+#
+#	cd ./luci-theme-argon/
+#
+#	sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" ./luci-app-argon-config/root/etc/config/argon
+#
+#	cd $PKG_PATH && echo "theme-argon has been fixed!"
+#fi
 
 #修改aurora菜单式样
 if [ -d *"luci-app-aurora-config"* ]; then
