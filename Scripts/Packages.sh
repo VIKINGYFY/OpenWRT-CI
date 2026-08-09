714c5453b8d3f854994c87787295c6308ac984ed
magiccloud-code <magiccloud89@gmail.com>
2026-08-09 13:28:23 +0800

    Add proton2025 theme package update

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -55,6 +55,7 @@ UPDATE_PACKAGE "kucat-config" "sirpdboy/luci-app-kucat-config" "master"
 UPDATE_PACKAGE "noobwrt" "nooblk-98/luci-theme-noobwrt" "master"
 UPDATE_PACKAGE "shadcn" "eamonxg/luci-theme-shadcn" "main"
 UPDATE_PACKAGE "theme-fluent" "LazuliKao/luci-theme-fluent" "main"
+UPDATE_PACKAGE "proton2025" "ChesterGoodiny/luci-theme-proton2025" "main"
 
 UPDATE_PACKAGE "momo" "nikkinikki-org/OpenWrt-momo" "main"
 UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
